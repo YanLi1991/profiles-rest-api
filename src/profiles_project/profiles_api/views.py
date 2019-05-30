f42ed18b1e24c4eb59848e7351c32ae137ab2778
unknown <bryanlibl20@gmail.com>
2019-05-29 23:17:14 -0400

    Added permissions and authentication to our userprofileviewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,8 +4,11 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
 from . import serializers
+from . import models
+from . import permissions
 
 # Create your views here.
 
@@ -98,3 +101,11 @@ class HelloViewSet(viewsets.ViewSet):
         """Handles removing part of an object"""
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
